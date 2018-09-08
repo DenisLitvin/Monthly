e708ab82d7c7e5fd7388cb29cd40a9269892e51b
Denis Litvin <den.litvinn@gmail.com>
2018-09-07 20:23:04 -0700

    New category style

--- Monthly/Extensions/UIColor+Theme.swift
+++ Monthly/Extensions/UIColor+Theme.swift
@@ -13,7 +13,7 @@ extension UIColor {
         static let background = UIColor(red: 23/255, green: 27/255, blue: 58/255, alpha: 1)
     }
     struct Theme {
-        static let blue = UIColor(red: 23/255, green: 52/255, blue: 216/255, alpha: 1)
+        static let blue = UIColor(red: 45/255, green: 112/255, blue: 217/255, alpha: 1)
         static let darkBlue = UIColor(red: 25/255, green: 31/255, blue: 75/255, alpha: 1)
         static let grayedBlue = UIColor(red: 66/255, green: 91/255, blue: 171/255, alpha: 1)
         static let lightBlue = UIColor(red: 161/255, green: 175/255, blue: 228/255, alpha: 1)
